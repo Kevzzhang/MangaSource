2383839984e952997b2c601d8a187785d7c54e08
Prothone <flashing.schlei3@gmail.com>
2016-06-27 15:09:01 +0700

    database

---
+++ Model/migrations/20160626154659_Chapter.js
@@ -0,0 +1,21 @@
+var table = function(table){
+	table.increments('id_chapter').primary();
+	table.string('namaChapter',50).notNullable();
+	table.datetime('waktu').notNullable();
+};
+
+
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('Chapter', table)
+		.then(function () {
+			console.log('Chapter table was created!');
+		});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema
+		.dropTable('Chapter', table)
+		.then(function () {
+			console.log('Chapter table was dropped!');
+		});  
+};
