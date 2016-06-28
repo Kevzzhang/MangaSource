a9e313512e6272098878344fe476d026bd134a41
linuxerdeb <linuxer.deb@gmail.com>
2016-06-28 15:26:59 +0700

    update utils admin dan penambahan utils book

--- Utils/ADMIN.js
+++ Utils/ADMIN.js
@@ -1,29 +1,9 @@
 var knex = require('../model');
 
-var createBook = function(Buku){
-		return knex('Buku').insert({
-			// cover : Buku.cover,
-			judul : Buku.title,
-			author : Buku.author,
-			rating : Buku.rating,
-			deskripsi : Buku.about
-
-		});
-}
-var findBOOK = function(judul){
-	return knex('Buku').select('id_buku','judul').where('judul',judul);
-};
-
-var fetchAllBook = function(){
-	return knex.select().table('Buku');
-}
-
 var findUSER = function(username){
 	return knex('admin').select('id_admin','username','password').where('username',username);
 };
 
 module.exports = {
-	findUSER:findUSER,
-	createBook : createBook,
-	findBOOK : findBOOK
+	findUSER:findUSER
 };
---
+++ Utils/BOOK.js
@@ -0,0 +1,30 @@
+var knex = require('../model');
+
+var createBook = function(Buku){
+		return knex('Buku').insert({
+			// cover : Buku.cover,
+			judul : Buku.title,
+			author : Buku.author,
+			rating : Buku.rating,
+			deskripsi : Buku.about
+
+		});
+}
+var findBOOK = function(judul){
+	return knex('Buku').select('id_buku','judul').where('judul',judul);
+};
+
+var fetchAllBook = function(){
+	return knex.select().table('Buku');
+}
+
+var searchTitle = function(query){
+	var requery = '%'+query+'%';
+	return knex('Buku').select('judul').where('judul','like',requery);
+}
+
+module.exports = {
+	createBook : createBook,
+	findBOOK : findBOOK,
+	searchTitle : searchTitle
+};
